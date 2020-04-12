39c4f247eedb9730e6c9b721faa9056764d2c837
KG <gbsosa@gmail.com>
2020-04-12 15:16:50 +0800

    Basic files added

---
+++ script.js
@@ -0,0 +1,31 @@
+/* Max of Two Numbers */
+// function max(a, b) {
+//     if (a > b) return a;
+//     else return b;
+// }
+
+// function max(a, b) {
+//     if (a > b) return a;
+//     return b;
+// }
+
+// Optimized code
+function max(a, b) {
+    return a > b ? a : b;
+}
+
+
+/* Landscape or Portrait */
+// function isLandscape(width, height) {
+//     if (width > height) return true;
+//     return false;
+// }
+
+// function isLandscape(width, height) {
+//     return width > height ? true : false;
+// }
+
+// Optimized code
+function isLandscape(width, height) {
+    return width > height;
+}
